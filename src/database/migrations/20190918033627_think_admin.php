bed6b2fb7ba166101bbaaa91385eb5e7363849fa
qian <806115620@qq.com>
2019-09-25 22:33:47 +0800

    修改数据库迁移

--- src/database/migrations/20190918033627_think_admin.php
+++ src/database/migrations/20190918033627_think_admin.php
@@ -42,7 +42,7 @@ class ThinkAdmin extends Migrator
             ->addColumn('access_token', 'string', ['comment' => '访问token', 'null' => true, 'length' => 32])
             ->addColumn('remember_token', 'string', ['comment' => '记住密码token', 'null' => true, 'length' => 60])
             ->addColumn('login_fail', 'integer', ['comment' => '登录失败次数', 'null' => true, 'default' => 0])
-            ->addColumn('supper', 'tinyint', ['comment' => '是否超级管理员：1是', 'null' => true, 'default' => 0])
+            ->addColumn('supper', 'integer', ['comment' => '是否超级管理员：1是', 'null' => true, 'default' => 0, 'length' => 1])
             ->addIndex(['username'], ['unique' => true])
             ->addIndex(['remember_token'])
             ->addIndex(['access_token'])

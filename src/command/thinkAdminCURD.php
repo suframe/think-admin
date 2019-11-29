5702a5a18591241bd8d45f1e6dcd5c4d5f53fd11
qian <806115620@qq.com>
2019-11-29 20:37:33 +0800

    去除表前缀

--- src/command/thinkAdminCURD.php
+++ src/command/thinkAdminCURD.php
@@ -3,6 +3,7 @@ declare (strict_types=1);
 
 namespace suframe\thinkAdmin\command;
 
+use Phinx\Db\Adapter\TablePrefixAdapter;
 use suframe\thinkAdmin\command\curd\Gen;
 use think\migration\Command;
 use think\console\Input;
@@ -24,12 +25,17 @@ class thinkAdminCURD extends Command
     protected function execute(Input $input, Output $output)
     {
         $table = trim($input->getArgument('table'));
+        /** @var TablePrefixAdapter $adapter */
+        $adapter = $this->getAdapter();
+        if ($table_prefix = $adapter->getOption('table_prefix')) {
+            $table = substr($table, strlen($table_prefix));
+        }
         $controller = $input->hasOption('controller') ? $input->getOption('controller') : null;
         $gen = new Gen();
         $gen->setOutput($output);
-        try{
-            $rs = $gen->build($this->getAdapter(), $table, $controller);
-            if($rs){
+        try {
+            $rs = $gen->build($adapter, $table, $controller);
+            if ($rs) {
                 $output->info('success');
             } else {
                 $output->info('fail');

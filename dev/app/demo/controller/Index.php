c9fdda68674877b1af0296f79551277c6f7c4ba0
shulinqian <330576744@qq.com>
2019-12-05 17:59:47 +0800

    增加sku

--- dev/app/demo/controller/Index.php
+++ dev/app/demo/controller/Index.php
@@ -3,7 +3,9 @@ declare (strict_types = 1);
 
 namespace app\demo\controller;
 
-use think\facade\Db;
+use FormBuilder\Driver\CustomComponent;
+use suframe\form\Form;
+use think\facade\View;
 
 /**
  * Class Index
@@ -16,11 +18,27 @@ class Index
     /**
      * @menu 菜单
      * @return string
+     * @throws \FormBuilder\Exception\FormBuilderException
+     * @throws \ReflectionException
      */
     public function index()
     {
-        $user = Db::table('admin_users');
-//        print_r($user->select());
-        return '您好！这是一个[demo]示例应用';
+
+        $form = (new Form)->createElm();
+        $form->setData([
+            'sku' => [
+                'title' => 'sssss'
+            ]
+        ]);
+        $form->setRuleByClass(\app\ui\form\NewsForm::class);
+        View::assign('form', $form);
+        $this->getThinkAdminViewLayout();
+        return View::fetch(config('thinkAdmin.view.commonForm'));
+    }
+
+    private function getThinkAdminViewLayout()
+    {
+        $layout = thinkAdminPath() . 'view' . DIRECTORY_SEPARATOR . 'layout_container.html';
+        View::assign('thinkAdminViewLayoutFile', $layout);
     }
 }
